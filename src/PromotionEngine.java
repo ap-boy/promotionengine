4024a20e5daf4a0f0eff7b33d90c13626cd0c177
Andrew Boy <andrewpeterboy@gmail.com>
2022-01-30 20:15:38 +0200

    Add calculation for Multiple of Promotion A

--- src/PromotionEngine.java
+++ src/PromotionEngine.java
@@ -5,16 +5,25 @@ public class PromotionEngine {
     private static int UNITPRICE_C = 20;
     private static int UNITPRICE_D = 15;
 
+    private static int PROMOTION_PRICE_A = 130;
+
     private int countActivePromotionA = 0;
     private int countActivePromotionB = 0;
     private int countActivePromotionC = 0;
     private int countActivePromotionD = 0;
 
+    private int numberActivePromotionA = 3;
+
     public int calculateTotalPrice(){
         int total = 0;
 
         if(countActivePromotionA > 0) {
-            total = total + (countActivePromotionA * UNITPRICE_A);
+            int additionalPromotionA = (countActivePromotionA % numberActivePromotionA);
+            if(additionalPromotionA > 0) {
+                total = total + (additionalPromotionA * UNITPRICE_A);
+                countActivePromotionA = countActivePromotionA - additionalPromotionA;
+            }
+            total = total + ((countActivePromotionA / numberActivePromotionA) * PROMOTION_PRICE_A);
         }
         if(countActivePromotionB > 0) {
             total = total + (countActivePromotionB * UNITPRICE_B);
