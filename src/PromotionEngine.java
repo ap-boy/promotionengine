c21e513c3c505ebf2660d4f95d3de21937ce7787
Andrew Boy <andrewpeterboy@gmail.com>
2022-01-30 19:47:00 +0200

    Refactor tests to pass Promotion A and Promotion B

--- src/PromotionEngine.java
+++ src/PromotionEngine.java
@@ -3,11 +3,20 @@ public class PromotionEngine {
     private static int UNITPRICE_A = 50;
     private static int UNITPRICE_B = 30;
 
-    private int countActivePromotionA;
-    private int countActivePromotionB;
+    private int countActivePromotionA = 0;
+    private int countActivePromotionB = 0;
 
     public int calculateTotalPrice(){
-        return countActivePromotionA * UNITPRICE_A;
+        int total = 0;
+
+        if(countActivePromotionA > 0) {
+            total = total + (countActivePromotionA * UNITPRICE_A);
+        }
+        if(countActivePromotionB > 0) {
+            total = total + (countActivePromotionB * UNITPRICE_B);
+        }
+
+        return total;
     }
 
     public void setCountActivePromotionA(int countActivePromotionA) {
